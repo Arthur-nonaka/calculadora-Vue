d583fdb8c8147d37ee6d395bd5f54bfd78045a06
Arthur <zenaboa7@gmail.com>
2023-04-05 11:43:56 -0300

    brasil

---
+++ my-app/pages/brasil.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul>
+        <li>Nome Comum: {{ this.dados[0].name.nativeName.por.common }}</li>
+        <li>Nome Oficial: {{ this.dados[0].name.nativeName.por.official }}</li>
+        <li>Moeda: {{ this.dados[0].currencies.BRL.name}}</li>
+        <li>Simbolo da Moeda: {{ this.dados[0].currencies.BRL.symbol}}</li>
+        <li>Capital: {{this.dados[0].capital[0]}}</li>
+        <li>Regiao: {{ this.dados[0].region}}</li>
+        <li>Sub-Regiao: {{ this.dados[0].subregion}}</li>
+        <li> Fronteiras: </li>
+        <ul v-for="border in this.dados[0].borders">
+            <li >
+                {{ border }}
+            </li>
+        </ul>
+    </ul>
+</template>
+
+<script>
+    import dados from "~/brasil.json";
+
+    export default {
+        data() {
+            return {
+                dados: dados,
+            }
+        }
+
+    }
+</script>
